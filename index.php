6aa6893b21a905c88f70f99cc18a4920276a513c
jonas-amilton <jonas96@edu.unisinos.br>
2024-02-18 00:19:05 -0300

    [refactor] pagina de login

--- index.php
+++ index.php
@@ -8,44 +8,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Prefeitura de São Leopoldo</title>
+    <title>Login</title>
 
     <link rel="stylesheet" href="css/style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <?php
+    include_once('config/url.php');
+    include_once('config/process.php');
+    ?>
 </head>
 
 <body>
 
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Prefeitura de São Leopoldo - Atendimento</span>
-        </div>
-    </nav>
+    <?php
+    include_once('templates/header.php');
+    ?>
 
     <div class="container-login">
-        <form class="card card-shadow login mt-2 d-flex flex-column" action="protocolo.php" method="post">
+        <form class="card card-shadow login mt-2 d-flex flex-column" action="<?= $BASE_URL ?>config/process.php"
+            method="post">
             <h3 class="text-center">
                 Abertura de Protocolos
                 <br>
                 <small class="text-body-secondary">Login</small>
             </h3>
             <label for="email">Digite seu email</label>
-            <input type="email" name="email" id="email" required>
+            <input type="email" name="email" id="email" placeholder="usuario@email.com" required>
             <label for="password">Digite sua senha</label>
-            <input type="password" name="password" id="password">
-            <input class="btn btn-primary mt-3" type="submit" value="enviar" required>
+            <input type="password" name="password" id="password" placeholder="********" required>
+            <button class="btn btn-primary mt-3" name="enviar" type="submit" value="login">
+                Enviar
+            </button>
+            <div class="text-center mt-2">
+                Ainda não tem conta?
+                <p><a class="link-opacity-75" href="pages/cadastro.php">Cadastre-se</a></p>
+            </div>
         </form>
-
     </div>
 
 
 
+    <?php
+    include_once('templates/footer.php');
+    ?>
 
-    <footer>
-        <p>Todos direitos reservados a Jonas Silva &reg RA:1803097</p>
-    </footer>
 
 </body>
 
