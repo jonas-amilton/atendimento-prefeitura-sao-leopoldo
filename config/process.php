899e934e54e018a627ecd92b6090b2e3c2564c66
jonas-amilton <jonas96@edu.unisinos.br>
2024-02-26 21:35:48 -0300

    [feat] verificação se o usuario esta logado antes de acessar pages/protocolo.php

--- config/process.php
+++ config/process.php
@@ -43,17 +43,17 @@ if (!empty($data)) {
         $email = $data['email'];
         $password = $data['password'];
         // print_r($data);
-        $stmt = $conn->prepare("SELECT * FROM users WHERE email = :email AND password = :password");
+        $stmt = $conn->prepare("SELECT id FROM users WHERE email = :email AND password = :password");
         $stmt->bindParam(":email", $email);
         $stmt->bindParam(":password", $password);
         $stmt->execute();
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        print_r($user);
+        // print_r($user);
 
         if ($user) {
             echo 'entrou aqui';
-            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['id'] = $user['id'];
             $_SESSION['email'] = $user['email'];
             header("Location:" . $BASE_URL . "../pages/protocolo.php");
             exit();
--- pages/protocolo.php
+++ pages/protocolo.php
@@ -1,3 +1,39 @@
 <?php
+// Inicia a sessão
+session_start();
 
-echo "<h1>Bem vindo</h1>";
+// Verifica se o usuário está logado
+if (!isset($_SESSION['id'])) {
+    // Se não estiver logado, redireciona para a página de login
+    header('Location: ../index.php');
+    exit();
+}
+
+// Se o usuário clicar no link de logout, limpa as variáveis de sessão e redireciona para a página de login
+if (isset($_GET['logout'])) {
+    // Limpa todas as variáveis de sessão
+    $_SESSION = array();
+    // Redireciona para a página de login
+    header("Location: ../index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Protocolos</title>
+</head>
+
+<body>
+    <!-- Link de logout -->
+    <a href="?logout=true">Logout</a>
+    <h1>Bem-vindo</h1>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, aliquid.
+        In saepe esse, laudantium accusamus ea sequi! Accusantium, mollitia omnis nesciunt eaque minus fuga eum, illo
+        odio quasi cumque quae?
+    </p>
+</body>
+
+</html>
