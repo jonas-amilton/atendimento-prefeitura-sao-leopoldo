5ca316c63a60354c3dbd7816e6654d252e8d32f0
Jonas Silva <jonas96@edu.unisinos.br>
2024-03-20 20:04:29 -0300

    [delete] apagando codigo em php puro para iniciar um novo com yii

--- templates/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>Todos direitos reservados a Jonas Silva &reg RA:1803097</p>
-</footer>
--- templates/header.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Prefeitura de São Leopoldo - Atendimento</span>
-    </div>
-</nav>
